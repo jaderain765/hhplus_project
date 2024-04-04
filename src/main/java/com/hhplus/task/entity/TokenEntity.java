3296bc86ab1586e589eecd79901bc7df579e8fe7
Jade_Rain <83251008+jaderain765@users.noreply.github.com>
2024-04-04 21:57:44 +0900

    업데이트

---
+++ src/main/java/com/hhplus/task/entity/TokenEntity.java
@@ -0,0 +1,30 @@
+package com.hhplus.task.entity;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.EntityListeners;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.ToString;
+import org.springframework.data.annotation.LastModifiedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+
+import java.time.LocalDateTime;
+
+@Entity
+@Table(name = "TOKEN")
+@Getter
+@NoArgsConstructor
+@EqualsAndHashCode
+@ToString
+@EntityListeners(AuditingEntityListener.class)
+public class TokenEntity {
+    @Id
+    private String tokenUUID;
+    private Long userId;
+
+    @LastModifiedDate
+    private LocalDateTime updateTime;
+}
