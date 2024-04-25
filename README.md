50e780b1076685fe97c6465595f8d8eedb256951
Jade_Rain <83251008+jaderain765@users.noreply.github.com>
2024-04-25 23:50:51 +0900

    수정

--- README.md
+++ README.md
@@ -1,5 +1,18 @@
 # hhplus_CI/CD_task_01
 
+## CI/CD
+
+### prod(5000)
+
+배포 서버 
+
+### test(8083)
+
+테스트 서버
+
+### dev(8082)
+
+개발 서버
 
 ## Docker
 
--- src/main/java/com/hhplus/task/TaskApplication.java
+++ src/main/java/com/hhplus/task/TaskApplication.java
@@ -1,8 +1,14 @@
 package com.hhplus.task;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.core.env.Environment;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 @EnableJpaAuditing
 @SpringBootApplication
@@ -12,4 +18,19 @@ public class TaskApplication {
 		SpringApplication.run(TaskApplication.class, args);
 	}
 
+}
+
+@Controller
+class TestController{
+
+	@Autowired
+	private Environment environment;
+
+	@ResponseBody
+	@GetMapping
+	public String test(){
+		String result = "설정 정보 : " + String.join("\n", environment.getActiveProfiles());
+		System.out.println(result);
+		return result;
+	}
 }
